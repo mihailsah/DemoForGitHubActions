4f2d3a4d4ec276efc7c682c79f33c4584daeea4e
Mihail Sahatchiev <mihail_sahatchiev@flutterint.com>
2022-06-21 13:40:57 +0300

    without role in backend

--- backend.tf
+++ backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "dtpl-terraform-state"
     key            = "dtpl-githubactions-poc"
     region         = "eu-west-1"
-    role_arn       = "arn:aws:iam::423432601656:oidc-provider/token.actions.githubusercontent.com"
     dynamodb_table = "dtpl-terraform-lock"
   }
 }
